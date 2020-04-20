fc884c33480d62f2d348e1954a9bab801732d563
f9n <f9n@protonmail.com>
2020-04-20 18:34:11 +0300

    Use profile for authentication

--- main.tf
+++ main.tf
@@ -2,9 +2,12 @@
 # CEN instance
 ###############
 provider "alicloud" {
-  version              = ">=1.56.0"
-  region               = var.region != "" ? var.region : null
-  configuration_source = "terraform-alicloud-modules/cen-instance"
+  version                 = ">=1.56.0"
+  profile                 = var.profile != "" ? var.profile : null
+  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
+  region                  = var.region != "" ? var.region : null
+  skip_region_validation  = var.skip_region_validation
+  configuration_source    = "terraform-alicloud-modules/cen-instance"
 }
 
 module "instance" {
