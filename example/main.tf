1eb71a84ed780fbcdf2975f88c48bb1e2c6587ac
Brian BORDINI <brian.bordini@richemont.com>
2020-08-10 17:24:43 +0200

    initial simplification of CEN module

--- example/main.tf
+++ example/main.tf
@@ -1,14 +1,24 @@
 module "cen" {
   source       = "../"
   name         = "cen-instance-name"
-  new_instance = true
+  description = "cen-example"
 
-  # instance_id            = "cen-xxx"
-  child_instance_id        = "vpc-xxx"
-  child_instance_region_id = "cn-xxx"
-  attach_instance          = true
-  route_table_id           = "vtb-xxx"
-  cidr_block               = "x.x.x.x/x"
-  publish_route_entry      = true
+  instances_attachment = [
+        {
+          "vpc_id": "vpc-abc123",
+          "vpc_name": "my-prod",
+          "vpc_owner_id": "123456",
+          "vpc_region_id": "cn-shanghai"
+        },
+        {
+          "vpc_id": "vpc-xyz987",
+          "vpc_name": "my-nonprod",
+          "vpc_owner_id": "098765",
+          "vpc_region_id": "cn-shanghai"
+        }
+  ]
+
+
+
 }
 
--- modules/instance_attachment/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "this_instance_id" {
-  value = var.instance_id
-}
-
-output "this_child_instance_id" {
-  value = var.child_instance_id
-}
-
